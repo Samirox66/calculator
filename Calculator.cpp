6c34df9276f5eb5095380ede7005694b2f6b8089
Samir <bsam@bk.ru>
2022-10-03 22:32:12 +0300

    add map of functions to reduce repeating code

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,6 +1,14 @@
 #include "Calculator.h"
 
-std::string Calculator::solve(std::string& expression)
+Calculator::Calculator()
+{
+	binaryFunctions["+"] = add;
+	binaryFunctions["-"] = subtract;
+	binaryFunctions["*"] = multiply;
+	binaryFunctions["/"] = divide;
+}
+
+std::string Calculator::solve(std::string& expression) const
 {
 	std::vector<std::string> reversePolskNotation;
 	if (!getReversePolskNotation(reversePolskNotation, expression))
@@ -16,24 +24,41 @@ void Calculator::readDll()
 
 }
 
-bool Calculator::getReversePolskNotation(std::vector<std::string>& reversePolskNotation, std::string const& expression)
+bool Calculator::getReversePolskNotation(std::vector<std::string>& reversePolskNotation, std::string const& expression) const
 {
 	std::stack<std::string> stack;
-	for (size_t i = 0; i < expression.size(); i++)
+	for (size_t i = 0; i < expression.size(); ++i)
 	{
-		if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/' || expression[i] == '(')
+		if (expression[i] >= '0' && expression[i] <= '9') {
+			std::string number = "";
+			bool hasPoint = false;
+			while (expression[i] >= '0' && expression[i] <= '9' || expression[i] == '.')
+			{
+				if (expression[i] == '.')
+				{
+					if (hasPoint)
+					{
+						return false;
+					}
+					hasPoint = true;
+				}
+				
+				number += std::string(1, expression[i]);
+				++i;
+			}
+
+			--i;
+			reversePolskNotation.push_back(number);
+		}
+		else if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/' || expression[i] == '(')
 		{
 			while (!stack.empty() && (expression[i] == '+' || expression[i] == '-') && (stack.top() == "*" || stack.top() == "/"))
 			{
 				reversePolskNotation.push_back(stack.top());
 				stack.pop();
 			}
-			stack.push(std::string(1, expression[i]));
-		}
-		else if (expression[i] >= '0' && expression[i] <= '9')
-		{
-			reversePolskNotation.push_back(std::string(1, expression[i]));
 
+			stack.push(std::string(1, expression[i]));
 		}
 		else if (expression[i] == ')')
 		{
@@ -42,10 +67,12 @@ bool Calculator::getReversePolskNotation(std::vector<std::string>& reversePolskN
 				reversePolskNotation.push_back(stack.top());
 				stack.pop();
 			}
+
 			if (stack.empty())
 			{
 				return false;
 			}
+
 			stack.pop();
 		}
 	}
@@ -59,50 +86,45 @@ bool Calculator::getReversePolskNotation(std::vector<std::string>& reversePolskN
 	return true;
 }
 
-double Calculator::solveReversePolskNotation(std::vector<std::string> const& reversePolskNotation)
+double Calculator::solveReversePolskNotation(std::vector<std::string> const& reversePolskNotation) const
 {
 	std::stack<double> stack;
-	std::optional<double> first;
-	std::optional<double> second;
 	for (auto& piece : reversePolskNotation)
 	{
-		if (piece == "+")
+		auto iter = binaryFunctions.find(piece);
+		if (iter != binaryFunctions.end())
 		{
 			double tmp = stack.top();
 			stack.pop();
-			tmp += stack.top();
-			stack.pop();
-			stack.push(tmp);
-		}
-		else if(piece == "-")
-		{
-			double tmp = stack.top();
-			stack.pop();
-			tmp = stack.top() - tmp;
-			stack.pop();
-			stack.push(tmp);
-		}
-		else if (piece == "*")
-		{
-			double tmp = stack.top();
-			stack.pop();
-			tmp *= stack.top();
-			stack.pop();
-			stack.push(tmp);
-		}
-		else if (piece == "/")
-		{
-			double tmp = stack.top();
-			stack.pop();
-			tmp = stack.top() / tmp;
+			tmp = iter->second(stack.top(), tmp);
 			stack.pop();
 			stack.push(tmp);
 		}
 		else
 		{
-			stack.push(std::stoi(piece));
+			stack.push(std::stod(piece));
 		}
 	}
 
 	return stack.top();
+}
+
+double Calculator::add(double a, double b)
+{
+	return a + b;
+}
+
+double Calculator::subtract(double a, double b)
+{
+	return a - b;
+}
+
+double Calculator::multiply(double a, double b)
+{
+	return a * b;
+}
+
+double Calculator::divide(double a, double b)
+{
+	return a / b;
 }
--- Calculator.h
+++ Calculator.h
@@ -2,15 +2,23 @@
 #include <string>
 #include <stack>
 #include <vector>
-#include <optional>
+#include <map>
+#include <functional>
 
 class Calculator
 {
 public:
+	Calculator();
 	void readDll();
-	std::string solve(std::string& expression);
+	std::string solve(std::string& expression) const;
 private:
-	bool getReversePolskNotation(std::vector<std::string>& reversePolskNotation, std::string const& expression);
-	double solveReversePolskNotation(std::vector<std::string> const& reversePolskNotation);
+	bool getReversePolskNotation(std::vector<std::string>& reversePolskNotation, std::string const& expression) const;
+	double solveReversePolskNotation(std::vector<std::string> const& reversePolskNotation) const;
+	static double add(double a, double b);
+	static double subtract(double a, double b);
+	static double multiply(double a, double b);
+	static double divide(double a, double b);
+private:
+	std::map <std::string, std::function<double(double, double)>> binaryFunctions;
 };
 
